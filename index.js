fbe6202e7fb7968045e83ded68f90423638264aa
Abhishek <57791251+AbhishekRPanicker@users.noreply.github.com>
2023-10-26 23:22:32 +0530

    Add buttons for more functionality

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-const boardSide = 500;
+const boardSide = 700;
 let boardSize = 16;
-let color = "#000";
+let currentColor = "#000000";
 let mouseHeld = false;
+let randomizeColor = false;
+let darkening = false;
 
+function changeSquareColor(event) {
+    if (randomizeColor) {
+        currentColor = `#${Math.floor(Math.random() * 0xffffff)}`;
+    }
 
-function changeColor(event) {
     if (event.target.id != "board" && (event.type === 'click' || (event.type === 'mouseover' && mouseHeld))) {
         let targetDiv = event.target;
-        targetDiv.style.backgroundColor = color;
-        console.log(event.target)
+        targetDiv.style.backgroundColor = currentColor;
+    }
+
+}
+
+function manageButtonEvent(event) {
+    let target = event.target;
+
+    switch (target.id) {
+        case "clear":
+            let squares = document.querySelectorAll(".square");
+            squares.forEach(square => {
+                square.style.backgroundColor = "#ffffff";
+            })
+            break;
+        case "random":
+            randomizeColor = true;
+            break;
+        case "reset":
+            randomizeColor = false;
+            currentColor = "#000000";
+            break;
+        case "erase":
+            currentColor = "#ffffff";
+            break;
+        case "darkening":
+            
+            break;
     }
 }
 
 
 function renderBoard() {
     let board = document.querySelector('#board');
-    board.addEventListener('click', changeColor);
-    board.addEventListener('mouseover', changeColor);
-    board.addEventListener('mousedown', e => mouseHeld = true);
-    board.addEventListener('mouseup', e => mouseHeld = false);
+    board.addEventListener('click', changeSquareColor);
+    board.addEventListener('mouseover', changeSquareColor);
+    document.addEventListener('mousedown', e => mouseHeld = true);
+    document.addEventListener('mouseup', e => mouseHeld = false);
+
 
     for (let i = 0; i < 16; i++) {
         let parentDiv = document.createElement("div");
@@ -35,3 +67,6 @@ function renderBoard() {
 }
 
 renderBoard()
+
+let buttonsDiv = document.querySelector(".buttons");
+buttonsDiv.addEventListener('click', manageButtonEvent)
